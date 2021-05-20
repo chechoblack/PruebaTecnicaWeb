54c78256d917a63ec9b55446e75f0276b887a928
chechoblack <andersonarguello13@gmail.com>
2021-05-20 09:51:32 -0600

    creacion de Entidades, DBEntiti, AccesoDatos se incluye el modelo de Vehiculo, para todo lo anterior funcionalidades, Solo muestra no tienene la capa logica

---
+++ PruebaTecnicaWeb/Controllers/VehiculoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PruebaTecnicaWeb.Entidades;
+using PruebaTecnicaWeb.AccesoDatos;
+namespace PruebaTecnicaWeb.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        // GET: Vehiculo
+        public ActionResult Index()
+        {
+            List<VehiculoClass> lst;
+            using (PruebaTecnicaEntities db = new PruebaTecnicaEntities())
+            {
+                lst = (from d in db.Vehiculo
+                       select new VehiculoClass
+                       {
+                           IdVehiculo = d.ID_Vehiculo,
+                           Placa = d.Placa,
+                           Dueno = d.Dueno,
+                           Marca = d.Marca
+
+                       }).ToList();
+
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ PruebaTecnicaWeb/Entidades/VehiculoClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaTecnicaWeb.Entidades
+{
+    public class VehiculoClass
+    {
+        public int IdVehiculo { get; set; }
+        public string Placa { get; set; }
+        public string Dueno { get; set; }
+        public string Marca { get; set; }
+    }
+}
